6b36186d549d5355c6c603be51ee2ee975956048
Eugene Chernov <40392032+ChernovDev1996@users.noreply.github.com>
2018-10-11 13:24:13 +0300

    Update script.js

--- Homework 2/Task 1/script.js
+++ Homework 2/Task 1/script.js
@@ -1,32 +1,20 @@
-// Does not work 1
 
-
-// var numberBefore = +prompt();
-
-// var numberAfter = numberBefore;
-
-
-// if (numberBefore) {
-//     for(i=numberBefore; i >= 0; i++) {
-//         console.log(numberBefore + numberAfter);
-//         if (i >= 100) break;
-//     }
-// }
+var number = +prompt();
 
 
 
+if (!isNaN(number)) {
+    if (number > 0) {
+        for(i = number; i <= 100; i++) {
+            console.log((i - 1) + (i - 2));
+        }
+    }
+    else {
+        console.log("More than 0, Sir")
+    } 
+}
 
 
-// Does not work 2 -Я уже даже в функции залез, help asap
-// var number = +prompt();
-// function factorial(number) {
-
-
-// if (number) {
-//     if (number <= 100) {
-//         for(i = number; i <= 100; i++) {
-//             console.log(number * factorial(number - 1));
-//         }
-//     }
-// }
-// }
+else {
+    console.log("Not anything other than num, please")
+}
