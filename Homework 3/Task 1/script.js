8e14bf7a4ca52993e763eda568b26ab22b325cc0
Eugene Chernov <40392032+ChernovDev1996@users.noreply.github.com>
2018-10-14 04:21:19 +0300

    Add files via upload

---
+++ Homework 3/Task 1/script.js
@@ -0,0 +1,60 @@
+
+// 2
+// var a = +prompt();
+// var b = a + a;
+
+// for (i = a; i <= b; b--) {
+//     console.log(b);
+// }
+
+
+
+// 3
+// var a = 2048;
+
+// min = 0;
+// max = 9;
+
+// for (i = min; i <= max; i++) {
+//     if (2048 - i == 2046) {
+//         console.log(i + " is the least number");
+//     }
+//     else if (2048 - i == 2040) {
+//         console.log(i + " the largest number")
+//     }
+// }
+
+
+
+
+// 4 - Тут у меня получился проблемный цикл. Не работает Тепло и Несите Азот, Ну Такое тоже походу - жду комментов.
+// var min = 0;
+// var max = 20;
+
+// var c = Math.floor(Math.random() * max) + min;
+
+
+
+// for (i = min; i <= max; i++) {
+//     if(max - c >= 4) {
+//         console.log("Холодно " + c);
+//         break;
+//     }
+
+//      else if(max - c <= 2&&3&&4) {
+//         console.log("Тепло " + c);
+//         break;
+
+//      }
+//      else if(max - c <= 1&&2) {
+//         console.log("Несите Азот " + c)
+//         break;
+
+//     }
+//     else if  (max - c == 0 || max - c == max) {
+//         console.log("Ну такое")}
+//     }
+    
+
+
+
