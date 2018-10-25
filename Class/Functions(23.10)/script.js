58799d27a52e75c495a8423ca5b0803bf45689a8
Eugene Chernov <40392032+ChernovDev1996@users.noreply.github.com>
2018-10-25 23:49:47 +0300

    Update Class/Functions(23.10)/script.js

---
+++ Class/Functions(23.10)/script.js
@@ -0,0 +1,27 @@
+
+function outputer (list, splitter) {
+    if(!splitter) {
+        splitter = " | ";
+    }
+
+    for(i = 0; i < list.length; i++) {
+        document.write(list[i] + splitter);
+    }
+}
+
+
+
+
+
+list = [1,2,3,4,5,6,7];
+outputer(array, " _ ");
+
+
+
+function filler () {
+
+
+
+}
+
+arr = new Array(n);
