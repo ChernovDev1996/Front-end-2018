ebda67d5d58098de6e2b98972027334fa417a2d6
Eugene Chernov <40392032+ChernovDev1996@users.noreply.github.com>
2018-11-04 21:02:43 +0200

    Update Stuff/Ajax/script.js

--- Stuff/Ajax/script.js
+++ Stuff/Ajax/script.js
@@ -1,3 +1,35 @@
-var ourRequest = new XMLHttpRequest();
 
-ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-2.json');
+
+var animalContainer =  document.getElementById("animal-info");
+
+var button = document.getElementById("button");
+
+
+
+
+button.addEventListener('click', function() {
+
+    var ourRequest = new XMLHttpRequest();
+
+    ourRequest.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+    
+    ourRequest.onload = function() {
+        var ourData = JSON.parse(ourRequest.responseText);
+        renderHTML(ourData);
+    };
+    
+    
+    ourRequest.send();
+});
+
+
+function renderHTML(data) {
+var htmlString = "";
+
+
+for (var i = 0; i < data.length; i++) {
+    htmlString += "<p>" + data[i].title + "</p>";
+}
+
+animalContainer.insertAdjacentHTML('beforeend', htmlString);
+}
