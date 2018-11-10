8f6748cc861feda6949ab0acad8d8f363b07bac1
Eugene Chernov <40392032+ChernovDev1996@users.noreply.github.com>
2018-11-10 23:56:31 +0200

    Gulp

---
+++ Stuff/Gulp/gulpfile.js
@@ -0,0 +1,67 @@
+var gulp = require("gulp"),
+    uglify = require("gulp-uglify"),
+    sass = require('gulp-sass'),
+    plumber = require('gulp-plumber'),
+    livereload = require('gulp-livereload'),
+    imgcompressor = require('gulp-imagemin');
+
+sass.compiler = require("node-sass");
+
+
+// function errorHandler(error) {
+//     console.error.bind(error);
+//     this.emit('end');
+// }
+
+// scripts task ugilifies
+gulp.task('scripts', function() {
+
+    gulp.src("./script.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./"))
+
+});
+
+
+// style task ugilifies
+gulp.task('styles', function() {
+
+    gulp.src("./style.css")
+    .pipe(uglify())
+    .pipe(gulp.dest("./"))
+});
+
+
+
+gulp.task('sass', function() {
+    gulp.src('./sass/**/*.scss')
+    .pipe(plumber())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'))
+    .pipe(livereload());
+});
+
+
+
+gulp.task('watch', function() {
+    livereload.listen();
+    gulp.watch('./script.js', ['scripts']);
+    gulp.watch('./style.css', ['styles']);
+});
+
+
+
+gulp.task('sass-watch', function() {
+    livereload.listen();
+    gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+
+gulp.task('imgcompressor', function() {
+    gulp.src('./img/*')
+        .pipe(imgcompressor())
+        .pipe(gulp.dest('./img'))
+    });
+
+
+gulp.task("default", ['scripts', 'styles', 'watch', 'sass'])
