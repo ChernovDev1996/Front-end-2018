a325d2dd957970d66440fe6938a5f8e00ee073d1
Eugene Chernov <40392032+ChernovDev1996@users.noreply.github.com>
2018-11-19 20:16:46 +0200

    Update script.js

--- Homework 8/script.js
+++ Homework 8/script.js
@@ -1,159 +1,35 @@
-// 1
-// function middleOfArgs() {
-
-//     var arrayForCentral = [];
-
-// for(i = 0; i < arguments.length; i++) {
-
-//     var middle = Math.ceil(arguments[i].length / 2) - 1;
-
-//     if(arguments[i].length % 2 ==0) {
-//         var even = arguments[i].splice(middle, 2);
-//         arrayForCentral.push(even);
-//     }
-
-//     else {
-//         var odd = arguments[i].splice(middle, 1);
-//         arrayForCentral.push(odd);
-//     }
-    
-// }
-
-// return arrayForCentral;
-
-// }
-
-
-// console.log(middleOfArgs([1,2,3,4,5], [1,2,3,4,5,6], [2,3,4,6,56,34,76], [2,3,4,6,56,34,76,75,98,121]));
-
-
-
-
-
-//2 
-
-//     function doFunction(number1, number2, func) {
-
-//         return func(number1, number2);
-// }
-
-// console.log(doFunction(4,4, function power(number1, number2) {
-//     return Math.pow(number1, number2);
-// }));
-
-
-
-// 3
-
-//     function doFunction(number1, number2, func) {
-//         var number1 = number1;
-//         var number2 = number2;
-
-//         return func(number1, number2);
-// }
-
-// doFunction(4,4, function power(number1, number2) {
-//     return console.log(Math.pow(number1, number2));
-// });
-
-// doFunction(4,4, function power(number1, number2) {
-//     return console.log(number1 * number2);
-// });
-
-// doFunction(4,4, function power(number1, number2) {
-//     return console.log(number1 / number2);
-// });
-
-
-// 4
-
-
-// V1
-
-//     function doFunction() {
-
-
-//         if(arguments.length == 3) {
-//          return func(arguments[0], arguments[1], arguments[2]);
-//         }
-
-//         else {
-//             return func(arguments[0], arguments[1]);
-//          }
-         
-// }
-
-// doFunction(4, factorial());
-
-// function factorial() {
-//     var factorial = 1;
-
-//     for(var i = 1; i <= arguments[0]; i++) {
-//         factorial = factorial * i;
-//     }
-
-//     console.log(factorial);
-
-// }
-
-
-// // // V2
-
-// function doFunction(number1, func) {
-
-//     if(arguments.length == 2 && typeof arguments[0] == !NaN) {
-//         return func(arguments[0], arguments[1]);
-//     }
-
-
-// }
-
-// doFunction(6, factorial());
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // Lection
 // 1
 
-object = {
-    x: 10,
-    y: 20,
-    inner: {
-        x: 20,
-        z: 30
-    },
-    foo2: {
-        k: 23,
-        p: 13
-    }
-}
+// object = {
+//     x: 10,
+//     y: 20,
+//     inner: {
+//         x: 20,
+//         z: 30
+//     },
+//     foo2: {
+//         k: 23,
+//         p: 13
+//     }
+// }
 
-function convert(obj) {
-    var newObj = {};
-    for (var key in obj){
-        if (typeof obj[key] == 'object') {
-            for (var key2 in obj[key]) {
-                newObj[key2] = obj[key][key2];
-            }
-        }else{
-            newObj[key] = obj[key];
-        }
-    }
-    return newObj;
-}
+// function convert(obj) {
+//     var newObj = {};
+//     for (var key in obj){
+//         if (typeof obj[key] == 'object') {
+//             for (var key2 in obj[key]) {
+//                 newObj[key2] = obj[key][key2];
+//             }
+//         }else{
+//             newObj[key] = obj[key];
+//         }
+//     }
+//     return newObj;
+// }
 
-console.log(convert(object));
+// console.log(convert(object));
 
 //2
 // var list = [1,2,3,4,45,5,6]
@@ -164,27 +40,73 @@ console.log(convert(object));
 
 //     var str = '';
 
-//     for(var i = 0; i < this.length - 1; i++) {
+//     for(var i = 0; i < this.length ; i++) {
 //         str += this[i];
 //     }
 //     return str;
 // }
 
 
-// list.myReverse = function() {
-//     this.reverse();
-// }
-
-
-// console.log(list.myReverse());
-
 
 //3
 
 
+// objectOne = {
+//     x: 10,
+//     y: 20,
+//     z: 40,
+//     n: 100,
+//     j: 25
+// }
 
-function assignObject(obj1, obj2) {
+
+// objectTwo = {
+//     a: 890,
+//     b: 30,
+//     t: 46,
+//     y: 29,
+//     n: 101,
+//     g: 999
+// }
 
 
-    
-}
+
+
+// function assignObject(obj1, obj2) {
+
+//     newObject = {};
+
+//     for (keyOne in obj1) {
+//         newObject[keyOne] = obj1[keyOne];
+//     }
+
+//     for (keyTwo in obj2) {
+//        newObject[keyTwo] = obj2[keyTwo]; 
+//     }
+
+//     return newObject;
+// }
+
+// console.log(assignObject(objectOne, objectTwo));
+
+
+
+
+//1 Home
+
+// function inArray(string, array) {
+
+//     for(var i = 0; i < array.length; i++) {
+//         if(string == array[i]) {
+//             return true;
+//         }
+
+//         else {
+//             return false;
+//         }
+//     }
+
+// }
+
+
+// console.log(inArray('hello', ['hello', 'bye']));
