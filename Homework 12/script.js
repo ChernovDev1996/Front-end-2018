4a34d53ccce5c156d1652ce74565c092792c3d38
Eugene Chernov <40392032+ChernovDev1996@users.noreply.github.com>
2018-12-05 18:20:44 +0200

    dz

--- Homework 12/script.js
+++ Homework 12/script.js
@@ -1,17 +1,16 @@
-window.onload = function () {
-
-
-
+window.onload = function() {
   function inputsTransfer() {
-    var inputs = document.querySelectorAll('.inputField');
-    var textField = document.querySelector('.textZone');
+    var inputs = document.querySelectorAll(".inputField");
+    var textField = document.querySelector(".textZone");
 
     for (var i = 0; i < inputs.length; i++) {
-      if (inputs[i].value !== '' && inputs[i] !== inputs[inputs.length - 1]) {
+      if (inputs[i].value !== "" && inputs[i] !== inputs[inputs.length - 1]) {
         textField.value += inputs[i].value + ",";
-
-      } else if (inputs[i] == inputs[inputs.length - 1]) {
-        textField.value += inputs[i].value + '.';
+      } else if (
+        inputs[i].value !== "" &&
+        inputs[i] == inputs[inputs.length - 1]
+      ) {
+        textField.value += inputs[i].value + ".";
       } else {
         return confirm("Enter the data please?");
       }
@@ -19,4 +18,4 @@ window.onload = function () {
   }
 
   setTimeout(inputsTransfer, 10000);
-}
+};
