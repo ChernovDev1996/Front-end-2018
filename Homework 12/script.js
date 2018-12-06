778be2b7a89c4a902010d4140f51709333d1f20e
Eugene Chernov <40392032+ChernovDev1996@users.noreply.github.com>
2018-12-06 20:00:12 +0200

    Rel

--- Homework 12/script.js
+++ Homework 12/script.js
@@ -1,21 +1,47 @@
 window.onload = function () {
+  // function inputsTransfer() {
+  //   var inputs = document.querySelectorAll(".inputField");
+  //   var textField = document.querySelector(".textZone");
+
+  //   for (var i = 0; i < inputs.length; i++) {
+  //     if (inputs[i].value !== "" && inputs[i] !== inputs[inputs.length - 1]) {
+  //       textField.value += inputs[i].value + ",";
+  //     } else if (
+  //       inputs[i].value !== "" &&
+  //       inputs[i] == inputs[inputs.length - 1]
+  //     ) {
+  //       textField.value += inputs[i].value + ".";
+  //     } else {
+  //       return confirm("Enter the data please?");
+  //     }
+  //   }
+  // }
+
+  // setInterval(inputsTransfer, 7000);
   function inputsTransfer() {
-    var inputs = document.querySelectorAll(".inputField");
+    var inputOne = document.querySelector("#inputFieldOne");
+    var inputTwo = document.querySelector("#inputFieldTwo");
+    var inputThree = document.querySelector("#inputFieldThree");
     var textField = document.querySelector(".textZone");
 
-    for (var i = 0; i < inputs.length; i++) {
-      if (inputs[i].value !== "" && inputs[i] !== inputs[inputs.length - 1]) {
-        textField.value += inputs[i].value + ",";
-      } else if (
-        inputs[i].value !== "" &&
-        inputs[i] == inputs[inputs.length - 1]
-      ) {
-        textField.value += inputs[i].value + ".";
-      } else {
-        return confirm("Enter the data please?");
-      }
+    var arrayInputs = [inputOne.value, inputTwo.value, inputThree.value];
+
+    if (inputOne !== "") {
+      textField.value = arrayInputs[0];
     }
+    else { console.log("Filled") }
+    if (inputOne !== "" && inputTwo !== "") {
+      textField.value = arrayInputs[0] + "\n" + arrayInputs[1];
+    }
+    else { console.log("Filled") }
+    if (inputOne !== "" && inputTwo !== "" && inputThree !== "") {
+      textField.value = arrayInputs[0] + "\n" + arrayInputs[1] + "\n" + arrayInputs[2];
+    }
+    else { console.log("Filled") }
   }
 
-  setInterval(inputsTransfer, 7000);
+  setInterval(inputsTransfer, 400);
+
 };
+
+
