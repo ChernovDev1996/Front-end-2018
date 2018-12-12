4233b1a7b5f3eb1d02f8eddaaa7bec69e6bc196f
Eugene Chernov <40392032+ChernovDev1996@users.noreply.github.com>
2018-12-12 17:35:36 +0200

    Class

--- Homework 13/script.js
+++ Homework 13/script.js
@@ -10,29 +10,24 @@ window.onload = function () {
 
 
   function blockDiagram(range, mainElement, innerElement, commission) {
-
+    commission.style.backgroundColor = 'red';
+    innerElement.style.backgroundColor = 'green';
     if (range == 100) {
       mainElement.style.height = 108 + 'px';
-      innerElement.style.height = range + 'px';
-      innerElement.style.backgroundColor = 'green';
     } else {
       mainElement.style.height = 100 + 'px';
       innerElement.style.height = range + 'px';
-      innerElement.style.backgroundColor = 'green';
     }
 
     if (range < 20) {
       commission.style.height = 2 + 'px';
-      commission.style.backgroundColor = 'red';
+
     } else if (range >= 20 && range <= 50) {
       commission.style.height = 4 + 'px';
-      commission.style.backgroundColor = 'red';
     } else if (range >= 50 && range <= 75) {
       commission.style.height = 6 + 'px';
-      commission.style.backgroundColor = 'red';
     } else if (range >= 75 && range <= 100) {
       commission.style.height = 8 + 'px';
-      commission.style.backgroundColor = 'red';
     }
 
   }
