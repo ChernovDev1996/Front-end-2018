355355c5d19d0304dbb9f1e12edde031bbfee8e5
Eugene Chernov <40392032+ChernovDev1996@users.noreply.github.com>
2018-12-13 12:17:41 +0200

    14

--- Homework 14/script.js
+++ Homework 14/script.js
@@ -1,90 +1,73 @@
 window.onload = function () {
-
-  // var inputRange = document.querySelector('.rangeInput');
-  // var rangeOutput = document.querySelector('.rangeRes');
-  // var creditBar = document.querySelector('.creditBar');
-  // var mainScale = document.querySelector('.main-scale')
-  // var creditScale = document.querySelector('.percentage-scale');
-  // var creditCommission = document.querySelector('.creditContents');
-
-
-
-  // function blockDiagram(range, mainElement, innerElement, commission) {
-  //   commission.style.backgroundColor = 'red';
-  //   innerElement.style.backgroundColor = 'green';
-  //   if (range == 100) {
-  //     mainElement.style.height = 108 + 'px';
-  //   } else {
-  //     mainElement.style.height = 100 + 'px';
-  //     innerElement.style.height = range + 'px';
-  //   }
-
-  //   if (range < 20) {
-  //     commission.style.height = 2 + 'px';
-
-  //   } else if (range >= 20 && range <= 50) {
-  //     commission.style.height = 4 + 'px';
-  //   } else if (range >= 50 && range <= 75) {
-  //     commission.style.height = 6 + 'px';
-  //   } else if (range >= 75 && range <= 100) {
-  //     commission.style.height = 8 + 'px';
-  //   }
-
-  // }
-
-  // inputRange.addEventListener('input', function (event) {
-  //   rangeOutput.value = event.target.value;
-  //   creditCommission.innerHTML = 'Credit: ' + event.target.value + " / Commission: " + creditScale.clientHeight;
-
-  //   blockDiagram(inputRange.value, creditBar, mainScale, creditScale);
-
-
-  // });
-
-  // rangeOutput.addEventListener('input', function (event) {
-  //   inputRange.value = event.target.value
-  //   creditCommission.innerHTML = 'Credit: ' + inputRange.value + " / Commission: " + creditScale.clientHeight;
-
-  //   blockDiagram(inputRange.value, creditBar, mainScale, creditScale);
-  // });
-
-
-
   function RenderHero(height, width, actionArea) {
     this.heroElem = document.createElement('div');
     this.heroElem.classList.add('heroElement');
     this.heroElem.style.height = height + 'px';
     this.heroElem.style.width = width + 'px';
+
+    this.heroWrap = document.createElement('div');
+    this.heroWrap.classList.add('heroWrap');
+
+    var xPos = 0;
+    var yPos = 0;
+
     console.log(this.heroElem);
 
     this.drawHero = function () {
-      actionArea.appendChild(this.heroElem);
+      this.heroWrap.appendChild(this.heroElem);
+      actionArea.appendChild(this.heroWrap);
       return this;
     }
 
     this.moveHero = function (keyCode, step) {
-      var heroLeft = 0;
-      var heroUp = 0;
-      var heroRight = 0;
-      var heroDown = 0;
       if (keyCode == 37) {
-        heroLeft += step;
-        console.log(newItem);
-        this.heroElem.style.right = heroLeft + "px";
+        xPos += step;
+        this.heroElem.style.right = xPos + "px";
       } else if (keyCode == 38) {
-        heroUp += step;
-        console.log(heroUp);
-        this.heroElem.style.top = heroUp + "px";
+        yPos -= step;
+        this.heroElem.style.top = yPos + "px";
       } else if (keyCode == 39) {
-        heroRight += step;
-        console.log(heroRight);
-        this.heroElem.style.left = heroRight + "px";
+        xPos -= step;
+        this.heroElem.style.right = xPos + "px";
       } else if (keyCode == 40) {
-        heroDown += step;
-        console.log(heroDown);
-        this.heroElem.style.bottom = heroDown + "px";
-      } else {
-        return "Blyat";
+        yPos += step;
+        this.heroElem.style.top = yPos + "px";
+      }
+      return this;
+    }
+
+    this.jumpHero = function (keyCode, jump) {
+      if (keyCode == 32) {
+        var curTop = this.heroElem.offsetTop;
+        this.heroElem.style.top = curTop - jump + 'px';
+        setTimeout(() => {
+          this.heroElem.style.top = curTop + 'px';
+        }, 300);
+      }
+      return this;
+    }
+
+
+    this.squezeHero = function (keyCode) {
+      if (keyCode == 17) {
+
+        var squezedHeight = this.heroElem.offsetHeight * 0.6;
+        var initHeight = this.heroElem.offsetHeight * 1.0;
+        var squezedWidth = this.heroElem.offsetWidth * 1.25;
+        var initWidth = this.heroElem.offsetWidth * 1.0;
+        this.heroElem.style.marginTop = '40px';
+        this.heroElem.style.height = squezedHeight + 'px';
+
+        setTimeout(() => {
+          this.heroElem.style.width = squezedWidth + 'px';
+        }, 50)
+
+
+        setTimeout(() => {
+          this.heroElem.style.marginTop = '0px';
+          this.heroElem.style.height = initHeight + 'px';
+          this.heroElem.style.width = initWidth + 'px';
+        }, 700);
       }
 
       return this;
@@ -94,49 +77,20 @@ window.onload = function () {
 
 
 
+
+
+
+
   var actionArea = document.querySelector('.wrapper');
-
   var newHero = new RenderHero(100, 100, actionArea);
-  console.log(newHero);
-
-
 
   document.addEventListener('keydown', function (event) {
-    var heroLeft = 0;
-    var heroUp = 0;
-    var heroRight = 0;
-    var heroDown = 0;
-
-    if (event.keyCode == 37) {
-      heroLeft += 2;
-      console.log(heroLeft);
-      newHero.heroElem.style.left = heroLeft + "px";
-    }
-    if (event.keyCode == 38) {
-      heroDown += 2;
-      console.log(heroDown);
-      newHero.heroElem.style.bottom = heroDown + "px";
-    }
-    if (event.keyCode == 39) {
-      heroRight += 2;
-      console.log(heroRight);
-      newHero.heroElem.style.right = heroRight + "px";
-    }
-    if (event.keyCode == 40) {
-      heroUp += 2;
-      console.log(heroUp);
-      newHero.heroElem.style.top = heroUp + "px";
-    }
-  });
-
-
-
-
-
-  document.addEventListener('keydown', function (event) {
-    if (event.keyCode == 13)
+    if (event.keyCode == 13) {
       newHero.drawHero();
+    };
+    newHero.moveHero(event.keyCode, 5);
+    newHero.jumpHero(event.keyCode, 100);
+    newHero.squezeHero(event.keyCode);
   });
-
 
 };
