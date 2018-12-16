15c10b01acc33a5c38bf2d2f6480161efdb9198f
Eugene Chernov <40392032+ChernovDev1996@users.noreply.github.com>
2018-12-16 18:44:43 +0200

    Da

--- Homework 14/script.js
+++ Homework 14/script.js
@@ -1,23 +1,23 @@
-window.onload = function () {
+window.onload = function() {
   function RenderHero(height, width, actionArea) {
-    this.heroElem = document.createElement('div');
-    this.heroElem.classList.add('heroElement');
-    this.heroElem.style.height = height + 'px';
-    this.heroElem.style.width = width + 'px';
+    this.heroElem = document.createElement("div");
+    this.heroElem.classList.add("heroElement");
+    this.heroElem.style.height = height + "px";
+    this.heroElem.style.width = width + "px";
 
-    this.heroWrap = document.createElement('div');
-    this.heroWrap.classList.add('heroWrap');
+    this.heroWrap = document.createElement("div");
+    this.heroWrap.classList.add("heroWrap");
 
     var xPos = 0;
     var yPos = 0;
 
-    this.drawHero = function () {
+    this.drawHero = function() {
       this.heroWrap.appendChild(this.heroElem);
       actionArea.appendChild(this.heroWrap);
       return this;
-    }
+    };
 
-    this.moveHero = function (keyCode, step) {
+    this.moveHero = function(keyCode, step) {
       if (keyCode == 37) {
         this.heroElem.style.backgroundColor = "yellow";
         xPos += step;
@@ -39,15 +39,15 @@ window.onload = function () {
         this.heroElem.style.backgroundColor = "green";
       }, 3000);
       return this;
-    }
+    };
 
-    this.jumpHero = function (keyCode, jump) {
+    this.jumpHero = function(keyCode, jump) {
       if (keyCode == 32) {
         var curTop = this.heroElem.offsetTop;
         this.heroElem.style.backgroundColor = "red";
-        this.heroElem.style.top = curTop - jump + 'px';
+        this.heroElem.style.top = curTop - jump + "px";
         setTimeout(() => {
-          this.heroElem.style.top = curTop + 'px';
+          this.heroElem.style.top = curTop + "px";
         }, 300);
       }
 
@@ -55,54 +55,51 @@ window.onload = function () {
         this.heroElem.style.backgroundColor = "green";
       }, 500);
       return this;
-    }
+    };
 
-
-    this.squezeHero = function (keyCode) {
+    this.squezeHero = function(keyCode) {
       if (keyCode == 17) {
-
         var squezedHeight = this.heroElem.offsetHeight * 0.6;
-        var initHeight = this.heroElem.offsetHeight * 1.0;
+
         var squezedWidth = this.heroElem.offsetWidth * 1.25;
-        var initWidth = this.heroElem.offsetWidth * 1.0;
+
         this.heroElem.style.backgroundColor = "blue";
-        this.heroElem.style.marginTop = '40px';
-        this.heroElem.style.height = squezedHeight + 'px';
+        this.heroElem.style.marginTop = "40px";
+        this.heroElem.style.height = squezedHeight + "px";
 
         setTimeout(() => {
-          this.heroElem.style.width = squezedWidth + 'px';
-        }, 50)
-
-
-        setTimeout(() => {
-          this.heroElem.style.marginTop = '0px';
-          this.heroElem.style.height = initHeight + 'px';
-          this.heroElem.style.width = initWidth + 'px';
-          this.heroElem.style.backgroundColor = "green";
-        }, 800);
+          this.heroElem.style.width = squezedWidth + "px";
+        }, 50);
       }
-
       return this;
-    }
+    };
+
+    this.unsquezeHero = function(keyCode) {
+      if (keyCode == 17) {
+        var initHeight = this.heroElem.offsetHeight * 1.0;
+        var initWidth = this.heroElem.offsetWidth * 1.0;
+        this.heroElem.style.marginTop = "0px";
+        this.heroElem.style.height = initHeight + "px";
+        this.heroElem.style.width = initWidth + "px";
+        this.heroElem.style.backgroundColor = "green";
+      }
+    };
   }
 
-
-
-
-
-
-
-
-  var actionArea = document.querySelector('.wrapper');
+  var actionArea = document.querySelector(".wrapper");
   var newHero = new RenderHero(100, 100, actionArea);
 
-  document.addEventListener('keydown', function (event) {
+  document.addEventListener("keydown", function(event) {
     if (event.keyCode == 13) {
       newHero.drawHero();
-    };
+    }
     newHero.moveHero(event.keyCode, 5);
     newHero.jumpHero(event.keyCode, 100);
-    newHero.squezeHero(event.keyCode);
+    document.addEventListener("keypress", function(event) {
+      newHero.squezeHero(event.keyCode);
+    });
+    document.addEventListener("keyup", function(event) {
+      newHero.unsquezeHero(event.keyCode);
+    });
   });
-
 };
