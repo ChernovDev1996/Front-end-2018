6b983d2ec9e38d7a96592d4ab9e29d437320195f
Eugene Chernov <40392032+ChernovDev1996@users.noreply.github.com>
2019-01-28 22:15:06 +0200

    cs

--- Homework Vue.js Diagrams/component.js
+++ Homework Vue.js Diagrams/component.js
@@ -1,10 +1,23 @@
 Vue.component('diagramcomponent', {
-    props: ['count'],
+    props: {
+        
+    },
     data() {
         return {
-            inputValue: null,
-            // instances: new Array(7),
+            inputValue: null, 
         }
+    },
+    methods: {
+        sendToLS(event) {
+            let inputValue = localStorage.setItem(`component-${this.id}`, event.target.value);
+            inputValue = localStorage.getItem(`component-${this.id}`)
+            this.inputValue = inputValue;
+            
+        },
+        
+    },
+    mounted() {
+        this.id = this._uid
     },
     template: `<div class="app__flex-wrap">
                 <div class="app__flex-wrap-diagram">
@@ -13,20 +26,52 @@ Vue.component('diagramcomponent', {
                     </div>
                 </div>
                 <input type="range" step="1" value="0" class="app__range-input"
-                    v-model="inputValue" 
+                    @input="sendToLS"
                 >
                 <p>{{inputValue}}</p>
+                
             </div>`
 });
+
+
+Vue.component('buttoncomponent', {
+    props: ['instancesDiagrams'],
+
+    data() {
+        return {
+            arrInstancesCompare: [],
+        }
+    },
+    methods: {
+        filterDiagrams() {
+           const diagrams = document.querySelectorAll('.diagramComp');
+           let diagramValue;
+           for(let i = 0; i < diagrams.length; i++) {
+                diagramValue = diagrams[i].querySelector('.app__range-input').value;
+                this.arrInstancesCompare.push(diagramValue);
+           }
+           this.instancesDiagrams.length = 0;
+           this.arrInstancesCompare.forEach(el => {
+               this.instancesDiagrams.push(el);
+           });
+           
+           let sortedInstancesDiagrams = this.instancesDiagrams.sort((a,b) => a - b);
+
+           const diagramBlocks = document.querySelectorAll('.app__flex-wrap-diagram-block'); 
+        }
+    },
+    template: `<button @click="filterDiagrams" id='btn'>Filter</button>`
+    })
 
 
 
 var app = new Vue({
     el: '#app',
     data: {
-        instances: new Array(7),
+        instances: new Array(7)
     },
     methods: {
+
     }
   
   
